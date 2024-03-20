52f984885d22271e44e0b9c36d057f7039d69443
mercyriall <mersiriall@gmail.com>
2024-03-21 01:35:22 +0300

    request parameters

--- utils/create_request_parameters.py
+++ utils/create_request_parameters.py
@@ -1,9 +1,12 @@
 import json
 
+_group_post_hash = "20b60f764fe0077f3d"
+_account_post_hash = "7dfa0bdfd13d4aa7be"
 
 def vk_account_post_details(payload, account_id, post_text):
     payload['to_id'] = account_id
     payload['Message'] = post_text
+    payload['hash'] = f'{_account_post_hash}'
     return payload
 
 
@@ -11,6 +14,7 @@ def vk_group_post_details(payload, group_id, post_text):
     payload['to_id'] = f"-{group_id}"
     payload['Message'] = post_text
     payload['_ads_group_id'] = group_id
+    payload['hash'] = f'{_group_post_hash}'
     return payload
 
 
