521e97db5b48578541c64cda2dae408800ef1038
Endz <ivanchukovarsen@yandex.ru>
2024-03-31 18:17:21 +0300

    123

--- main.py
+++ main.py
@@ -30,6 +30,5 @@ async def main():
 
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
-
 if __name__ == '__main__':
     asyncio.run(main())
--- utils/check_link.py
+++ utils/check_link.py
@@ -3,10 +3,10 @@ from aiogram.types import Message
 from database.init_db import database as db
 
 
-async def check_linked_soc(msg: Message):
+async def check_linked_soc_list(msg: Message):
     user = await db.get_data_user(msg.from_user.id)
 
-    usr_list = list(user[1:])
+    usr_list = list(user)
 
     networks = []
 
@@ -16,3 +16,22 @@ async def check_linked_soc(msg: Message):
         else:
             networks.append(0)
     return networks
+
+
+async def check_for_buttons(msg: Message):
+    user = await db.get_data_user(msg.from_user.id)
+
+    usr_list = list(user)
+
+    networks_dict ={}
+
+    networks = ["Вконтакте", "Twitter", "Telegram"]
+
+# 1 - если привязана, 2 - если не привязана, 0 - если отменят
+    for i in range(3):
+        if bool(usr_list[i]) is False:
+            networks_dict[networks[i]] = 2
+        else:
+            networks_dict[networks[i]] = 1
+
+    return networks_dict
